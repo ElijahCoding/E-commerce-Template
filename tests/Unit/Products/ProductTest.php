fabf0d663db137df6832eec511da6420c9a90d9d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-11 18:07:29 +0300

    building products

---
+++ tests/Unit/Products/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Products;
+
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
