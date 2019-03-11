20c4442744a10a88c26f56a7face01da476cca53
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-11 17:24:46 +0300

    categories tests

--- tests/Feature/Categories/CategoryIndexTest.php
+++ tests/Feature/Categories/CategoryIndexTest.php
@@ -19,4 +19,33 @@ class CategoryIndexTest extends TestCase
             ]);
         });
     }
+
+    public function test_it_returns_only_parent_categories()
+    {
+        $category = factory(Category::class)->create();
+
+        $category->children()->save(
+            factory(Category::class)->create()
+        );
+
+        $this->json('GET', 'api/categories')
+             ->assertJsonCount(1, 'data');
+    }
+
+    public function test_it_returns_categories_ordered_by_their_given_order()
+    {
+        $category = factory(Category::class)->create([
+            'order' => 2
+        ]);
+
+        $anotherCategory = factory(Category::class)->create([
+            'order' => 1
+        ]);
+
+        $this->json('GET', 'api/categories')
+             ->assertSeeInOrder([
+                 $anotherCategory->slug,
+                 $category->slug
+             ]);
+    }
 }
