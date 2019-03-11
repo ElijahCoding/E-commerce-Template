ac029b3cd2dd73d5087601e21be2be96fb96dda7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-11 18:21:55 +0300

    product index endpoint test

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Products;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductIndexResource;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::get();
-        
+        $products = Product::paginate(10);
+
+        return ProductIndexResource::collection($products);
     }
 }
---
+++ tests/Feature/Products/ProductIndexTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Tests\TestCase;
+
+class ProductIndexTest extends TestCase
+{
+    public function test_it_shows_a_collection_of_products()
+    {
+        
+    }
+}
