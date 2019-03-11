b7c4507c0bebb3af3f409050225a8cba668ba8b2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-11 18:57:38 +0300

    product index test

--- tests/Feature/Products/ProductIndexTest.php
+++ tests/Feature/Products/ProductIndexTest.php
@@ -3,11 +3,25 @@
 namespace Tests\Feature\Products;
 
 use Tests\TestCase;
+use App\Models\Product;
 
 class ProductIndexTest extends TestCase
 {
     public function test_it_shows_a_collection_of_products()
     {
-        
+        $product = factory(Product::class)->create();
+
+        $this->json('GET', 'api/products')
+             ->assertJsonFragment([
+                 'id' => $product->id
+             ]);
+    }
+
+    public function test_it_has_paginated_data()
+    {
+        $this->json('GET', 'api/products')
+             ->assertJsonStructure([
+                 'data', 'meta', 'links'
+             ]);
     }
 }
