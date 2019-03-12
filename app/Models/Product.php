4c68e8aecbf1865a99a830d23ad2e45792b8e1ad
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 16:55:03 +0300

    building money

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,18 +2,32 @@
 
 namespace App\Models;
 
+use Money\Money;
+use Money\Currency;
+use NumberFormatter;
 use App\Models\Category;
 use App\Models\ProductVariation;
-use App\Models\Traits\CanBeScoped;
+use Money\Formatter\IntlMoneyFormatter;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\{CanBeScoped, HasPrice};
 
 class Product extends Model
 {
     use CanBeScoped;
-    
+
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function getPriceAttribute($value)
+    {
+        return new Money($value, new Currency('USD'));
+    }
+
+    public function getFormattedPriceAttribute()
+    {
+
     }
 
     public function categories()
