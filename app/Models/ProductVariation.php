cdc397e063644990cdc9b2729c9b936b0cc56e51
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 17:52:54 +0300

    inherit from product price if it is null

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use App\Cart\Money;
 use App\Models\Product;
+use App\Models\Traits\HasPrice;
 use App\Models\ProductVariationType;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductVariation extends Model
 {
+    use HasPrice;
+
+    public function getPriceAttribute($value)
+    {
+        if ($value === null) {
+            return $this->product->price;
+        }
+
+        return new Money($value);
+    }
+
     public function type()
     {
         return $this->hasOne(ProductVariationType::class, 'id', 'product_variation_type_id');
