a02a2fa82499cd8a5450b7f75b3f5a02378f01ec
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 12:28:58 +0300

    product variation type

--- app/Http/Resources/ProductVariationResource.php
+++ app/Http/Resources/ProductVariationResource.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Support\Collection;
+use App\Http\Resources\ProductVariationResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductVariationResource extends JsonResource
 {
     public function toArray($request)
     {
+        if ($this->resource instanceof Collection) {
+            return ProductVariationResource::collection($this->resource);
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name
