c57c8b2a6d8abf7d1b51610db0e482d4b5998879
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 12:13:36 +0300

    product variation

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -3,13 +3,14 @@
 namespace App\Http\Resources;
 
 use App\Http\Resources\ProductIndexResource;
+use App\Http\Resources\ProductVariationResource;
 
 class ProductResource extends ProductIndexResource
 {
     public function toArray($request)
     {
         return array_merge(parent::toArray($request), [
-            'variations' => []
+            'variations' => ProductVariationResource::collection($this->variations)
         ]);
     }
 }
