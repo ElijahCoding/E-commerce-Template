4652a04f78c8a91a11f8b56a07f4bad845e85380
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 14:26:44 +0300

    scope trait

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,22 +2,18 @@
 
 namespace App\Models;
 
-use App\Scoping\Scoper;
 use App\Models\Category;
 use App\Models\ProductVariation;
+use App\Models\Traits\CanBeScoped;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
+    use CanBeScoped;
+    
     public function getRouteKeyName()
     {
         return 'slug';
-    }
-
-    public function scopeWithScopes(Builder $builder, $scopes = [])
-    {
-        return (new Scoper(request()))->apply($builder, $scopes);
     }
 
     public function categories()
