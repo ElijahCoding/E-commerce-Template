1268daddfa066f5c13cb4e94a10a00ec6c5ad5f8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-12 10:21:55 +0300

    show product with tests

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ProductIndexResource;
 
-class ProductResource extends JsonResource
+class ProductResource extends ProductIndexResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return array_merge(parent::toArray($request), [
+            'variations' => []
+        ]);
     }
 }
