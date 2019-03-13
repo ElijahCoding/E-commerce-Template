6125ab928a9b2a4e2356e7e70bf00333e99200c5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-13 15:28:40 +0300

    quantity in view built

--- public/sql_views/product_variation_stock_view.sql
+++ public/sql_views/product_variation_stock_view.sql
@@ -4,7 +4,7 @@ CREATE VIEW product_variation_stock_view AS
     SELECT
         product_variations.product_id AS product_id,
         product_variations.id AS product_variation_id,
-        COALESCE(SUM(stocks.quantity), 0) as stock
+        COALESCE(SUM(stocks.quantity) - COALESCE(SUM(product_variation_order.quantity), 0), 0) as stock
     FROM product_variations
     LEFT JOIN (
         SELECT stocks.product_variation_id as id,
@@ -12,4 +12,11 @@ CREATE VIEW product_variation_stock_view AS
         FROM stocks
         GROUP BY stocks.product_variation_id
     ) AS stocks USING (id)
+    LEFT JOIN (
+        SELECT
+            product_variation_order.product_variation_id as id,
+            SUM(product_variation_order.quantity) as quantity
+        FROM product_variation_order
+        GROUP BY product_variation_order.product_variation_id
+    ) AS product_variation_order USING (id)
     GROUP BY product_variations.id
