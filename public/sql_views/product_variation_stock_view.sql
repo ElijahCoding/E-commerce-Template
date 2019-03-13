babdb11a28b7e8651902d9ee42dee2dce465954e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-13 14:51:34 +0300

    view creating

--- public/sql_views/product_variation_stock_view.sql
+++ public/sql_views/product_variation_stock_view.sql
@@ -1,1 +1,15 @@
 DROP VIEW IF EXISTS product_variation_stock_view;
+
+CREATE VIEW product_variation_stock_view AS
+    SELECT
+        product_variations.product_id AS product_id,
+        product_variations.id AS product_variation_id,
+        COALESCE(SUM(stocks.quantity), 0) as stock
+    FROM product_variations
+    LEFT JOIN (
+        SELECT stocks.product_variation_id as id,
+        SUM(stocks.quantity) as quantity
+        FROM stocks
+        GROUP BY stocks.product_variation_id
+    ) AS stocks USING (id)
+    GROUP BY product_variations.id
