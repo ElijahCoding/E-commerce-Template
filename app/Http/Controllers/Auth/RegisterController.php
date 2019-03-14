79c12a620a477ee6dd0d1932cfb6e798afbf389d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-14 14:16:36 +0300

    building registerccontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\PrivateUserResource;
 
 class RegisterController extends Controller
 {
-    public function action()
+    public function action(Request $request)
     {
-
+        $user = User::create($request->only('email', 'name', 'password'));
     }
 }
